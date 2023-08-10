d10b66d4d3cb8606c28898dddb5330074cef7285
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-08-10 10:38:09 -0700

    TitleToNumber

--- Program.cs
+++ Program.cs
@@ -1,8 +1,55 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
 
-string s = "PAYPALISHIRINGHERE"; int rows = 4;
-Console.WriteLine(Convert(s, rows));
+string s = "AB";
+Console.WriteLine(TitleToNumber(s));
+
+static int TitleToNumber(string columnTitle)
+{
+    var dic = new Dictionary<char, int>() 
+    {
+        { 'A', 1},
+        { 'B', 2},
+        { 'C', 3},
+        { 'D', 4},
+        { 'E', 5},
+        { 'F', 6},
+        { 'G', 7},
+        { 'H', 8},
+        { 'I', 9},
+        { 'J', 10},
+        { 'K', 11},
+        { 'L', 12},
+        { 'M', 13},
+        { 'N', 14},
+        { 'O', 15},
+        { 'P', 16},
+        { 'Q', 17},
+        { 'R', 18},
+        { 'S', 19},
+        { 'T', 20},
+        { 'U', 21},
+        { 'V', 22},
+        { 'W', 23},
+        { 'X', 24},
+        { 'Y', 25},
+        { 'Z', 26},
+    };
+    int result = 0;
+    
+    for (int i = 0; i < columnTitle.Length; i++)
+    {
+        int t = columnTitle.Length - i;
+        int k = 1;
+        while (t > 1)
+        {
+            k *= 26;
+            t--;
+        }
+        result += k * dic[columnTitle[i]];
+    }
+    return result;
+}
 
 static string Convert(string s, int numRows)
 {
@@ -12,7 +59,7 @@ static string Convert(string s, int numRows)
     int currRow = 0;
     int currCol = 0;
     bool diag = false;
-    while (index < s.Length - 1)
+    while (index < s.Length)
     {
 
         if (currRow == numRows)
