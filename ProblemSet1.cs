929ef7722491492d7f7732c46cfc9daf5a90e216
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-08-24 12:55:58 -0700

    IslandPerimeter

--- ProblemSet1.cs
+++ ProblemSet1.cs
@@ -10,13 +10,52 @@ namespace ConsoleApp1
 {
     internal class ProblemSet1
     {
-        public static int MaxAreaOfIsland(int[][] grid)
-        {
-            var directions = new int[][] {
+        private static int[][] directions = new int[][] {
             new int[] {-1, 0},
             new int[] {0, 1},
             new int[] {1, 0},
             new int[] {0, -1}};
+        public static int IslandPerimeter(int[][] grid)
+        {
+            Queue<int[]> queue = new Queue<int[]>();
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 1) queue.Enqueue(new int[] { i, j });
+                }
+            }
+
+            int perimeter = 0;
+            while (queue.Count > 0)
+            {
+                var currentCoordinates = queue.Dequeue();
+                int numberOfWalls = 0;
+                for (int k = 0; k < directions.Length; k++)
+                {
+
+                    var currentRow = currentCoordinates[0];
+                    var currentCol = currentCoordinates[1];
+                    var direction = directions[k];
+                    var nextRow = currentRow + direction[0];
+                    var nextCol = currentCol + direction[1];
+                    if (nextRow < 0 || nextRow >= grid.Length || nextCol < 0 || nextCol >= grid[0].Length)
+                    {
+                        numberOfWalls++;
+                        continue;
+                    }
+                    if (grid[nextRow][nextCol] == 0)
+                        numberOfWalls++;
+
+                }
+                perimeter += numberOfWalls;
+            }
+
+            return perimeter;
+        }
+
+        public static int MaxAreaOfIsland(int[][] grid)
+        {
             Queue<int[]> queue = new Queue<int[]>();
             int maxArea = 0;
             for (int i = 0; i < grid.Length; i++)
@@ -33,7 +72,7 @@ namespace ConsoleApp1
                             var currentCoordinates = queue.Dequeue();
                             for (int k = 0; k < directions.Length; k++)
                             {
-                                
+
                                 var currentRow = currentCoordinates[0];
                                 var currentCol = currentCoordinates[1];
                                 var direction = directions[k];
@@ -51,7 +90,7 @@ namespace ConsoleApp1
                             }
                         }
 
-                        if (currentArea > maxArea) maxArea= currentArea;
+                        if (currentArea > maxArea) maxArea = currentArea;
                     }
                 }
             }
--- Program.cs
+++ Program.cs
@@ -5,14 +5,10 @@ using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
 
-int[][] grid = { 
-    new int[] { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
-    new int[] { 0,0,0,0,0,0,0,1,1,1,0,0,0 },
-    new int[] {0,1,1,0,1,0,0,0,0,0,0,0,0 },
-    new int[] {0,1,0,0,1,1,0,0,1,0,1,0,0 },
-    new int[] {0,1,0,0,1,1,0,0,1,1,1,0,0 },
-    new int[] {0,0,0,0,0,0,0,0,0,0,1,0,0 },
-    new int[] {0,0,0,0,0,0,0,1,1,1,0,0,0 },
-    new int[] {0,0,0,0,0,0,0,1,1,0,0,0,0 } };
-Console.WriteLine(ProblemSet1.MaxAreaOfIsland(grid));
+int[][] grid = {
+    new int[] { 0, 1, 0, 0 },
+    new int[]{1, 1, 1, 0 },
+    new int[]{0, 1, 0, 0 },
+    new int[]{1, 1, 0, 0 }};
+Console.WriteLine(ProblemSet1.IslandPerimeter(grid));
 
