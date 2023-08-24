1e99db364f4f27c144399ea08531419899b906e0
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-08-24 13:26:31 -0700

    NumIslands

--- ProblemSet1.cs
+++ ProblemSet1.cs
@@ -15,6 +15,46 @@ namespace ConsoleApp1
             new int[] {0, 1},
             new int[] {1, 0},
             new int[] {0, -1}};
+
+        public static int NumIslands(char[][] grid)
+        {
+            Queue<int[]> queue = new Queue<int[]>();
+            int numberOfIslands = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        numberOfIslands += 1;
+                        grid[i][j] = '0';
+                        queue.Enqueue(new int[] { i, j });
+                        while (queue.Count != 0)
+                        {
+                            var currentCoordinates = queue.Dequeue();
+                            for (int k = 0; k < directions.Length; k++)
+                            {
+
+                                var currentRow = currentCoordinates[0];
+                                var currentCol = currentCoordinates[1];
+                                var direction = directions[k];
+                                var nextRow = currentRow + direction[0];
+                                var nextCol = currentCol + direction[1];
+                                if (nextRow < 0 || nextRow >= grid.Length || nextCol < 0 || nextCol >= grid[0].Length)
+                                    continue;
+
+                                if (grid[nextRow][nextCol] == '1')
+                                {
+                                    grid[nextRow][nextCol] = '0';
+                                    queue.Enqueue(new int[] { nextRow, nextCol });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return numberOfIslands;
+        }
         public static int IslandPerimeter(int[][] grid)
         {
             Queue<int[]> queue = new Queue<int[]>();
--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
 
-int[][] grid = {
-    new int[] { 0, 1, 0, 0 },
-    new int[]{1, 1, 1, 0 },
-    new int[]{0, 1, 0, 0 },
-    new int[]{1, 1, 0, 0 }};
-Console.WriteLine(ProblemSet1.IslandPerimeter(grid));
+char[][] grid = {
+    new char[] {'1','1','0','0','0'},
+    new char[] {'1','1','0','0','0'},
+    new char[] {'0','0','1','0','0'},
+    new char[] {'0','0','0','1','1'}
+};
+
+
+Console.WriteLine(ProblemSet1.NumIslands(grid));
 
