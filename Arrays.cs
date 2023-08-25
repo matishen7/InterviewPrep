e9a032db08cf3b711438f1f94ab749f432ca4cd0
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-08-25 10:11:33 -0700

    MaxProfit

--- Arrays.cs
+++ Arrays.cs
@@ -15,9 +15,26 @@ namespace ConsoleApp1
             {
                 int ff = target - arr[i];
                 if (dic.ContainsKey(ff)) return new int[] { dic[ff], i };
-                else (!dic.ContainsKey(arr[i])) dic.Add(arr[i], i);
+                else if (!dic.ContainsKey(arr[i])) dic.Add(arr[i], i);
             }
             return null;
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+            int min = prices[0];
+            int max = prices[prices.Length - 1];
+            int j = prices.Length - 1;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (min > prices[i]) min = prices[i];
+                if (max < prices[j]) max = prices[j];
+                if (profit < max - min) profit = max - min;
+                j--;
+                if (j <= i) break;
+            }
+            return profit;
         }
     }
 }
