1442bc205f947cfee8a7671cc6b9dfb317a29a4f
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-05 10:31:39 -0700

    Insert

--- InterVals.cs
+++ InterVals.cs
@@ -38,5 +38,37 @@ namespace ConsoleApp1
 
             return result.ToArray();
         }
+
+        public static int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            var newIntervals = intervals.ToList();
+            newIntervals.Add(newInterval);
+            intervals = newIntervals.OrderBy(innerArr => innerArr[0]).ToArray();
+            List<int[]> result = new List<int[]>();
+            result.Add(intervals[0]);
+            int currentInterval = 0;
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                var start = intervals[i][0];
+                var end = intervals[i][1];
+                var currentStart = result[currentInterval][0];
+                var currentEnd = result[currentInterval][1];
+
+                if (currentEnd < start)
+                {
+                    result.Add(intervals[i]);
+                    currentInterval++;
+                }
+                else
+                {
+                    int newStart = (currentStart < start) ? currentStart : start;
+                    int newEnd = (currentEnd <= end) ? end : currentEnd;
+                    result[currentInterval][0] = newStart;
+                    result[currentInterval][1] = newEnd;
+                }
+            }
+
+            return result.ToArray();
+        }
     }
 }
