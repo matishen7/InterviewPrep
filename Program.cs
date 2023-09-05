72f381ddcff43ab73c5c64b1e81dea6233adb811
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-05 13:47:32 -0700

    LengthOfLongestSubstring

--- Program.cs
+++ Program.cs
@@ -4,5 +4,5 @@ using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
-int[][] intervals = { new int[] { 1, 2 },  new int[] { 2, 3 }, new int[] { 3, 4 } , new int[] { 1, 3 }, };
-Console.WriteLine(InterVals.EraseOverlapIntervals(intervals));
+var str = "a";
+Console.WriteLine(StringProblems.LengthOfLongestSubstring(str));
---
+++ StringProblems.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StringProblems
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0) return 0;
+            int maxCount = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var set = new HashSet<char>();
+                int count = 0;
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (!set.Contains(s[j]))
+                    {
+                        set.Add(s[j]);
+                        count++;
+                    }
+                    else break;
+
+                    if (maxCount < count) maxCount = count;
+
+                }
+            }
+
+            return maxCount;
+        }
+    }
+}
