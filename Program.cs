35f470c934f7e73fbf93c72b3b2f43b3f8066b28
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-06 10:15:59 -0700

    IsValid

--- Program.cs
+++ Program.cs
@@ -4,5 +4,5 @@ using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
-var str = "a";
-Console.WriteLine(StringProblems.LengthOfLongestSubstring(str));
+string str = "A", t = "A";
+Console.WriteLine(StringProblems.MinWindow(str, t));
--- StringProblems.cs
+++ StringProblems.cs
@@ -8,6 +8,38 @@ namespace ConsoleApp1
 {
     internal class StringProblems
     {
+
+        public static bool IsValid(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return true;
+            var stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                var cc = s[i];
+                if (cc == '{' || cc == '[' || cc == '(') stack.Push(cc);
+                else if (cc == '}')
+                {
+                    var last = stack.Peek();
+                    if (last == '{') stack.Pop();
+                    else stack.Push(last);
+                }
+                else if (cc == ']')
+                {
+                    var last = stack.Peek();
+                    if (last == '[') stack.Pop();
+                    else stack.Push(last);
+                }
+                else if (cc == ')')
+                {
+                    var last = stack.Peek();
+                    if (last == '(') stack.Pop();
+                    else stack.Push(last);
+                }
+            }
+
+            return (stack.Count > 0) ? false : true;
+        }
+
         public static int LengthOfLongestSubstring(string s)
         {
             if (s.Length == 0) return 0;
@@ -31,6 +63,36 @@ namespace ConsoleApp1
             }
 
             return maxCount;
+        }
+
+        public static string MinWindow(string s, string t)
+        {
+            if (t.Length > s.Length) return "";
+            int w = t.Length;
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j <= s.Length - w; j++)
+                {
+                    var currentStr = s.Substring(j, w);
+                    if (ContainsAllchars(currentStr, t)) { return currentStr; }
+                }
+
+                w++;
+
+            }
+
+            return "";
+
+        }
+
+        public static bool ContainsAllchars(string str, string t)
+        {
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (!str.Contains(t[i])) return false;
+            }
+
+            return true;
         }
     }
 }
