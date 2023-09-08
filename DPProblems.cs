41ef4e18cdc8f7ed7112306d2c3da5134744e1b0
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-08 13:46:52 -0700

    MaxProfit

---
+++ DPProblems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class DPProblems
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            if (prices.Length <= 1) return 0;
+            int minSoFar = prices[0];
+            int profit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                var currentProfit = prices[i] - minSoFar;
+                if (profit < currentProfit) profit = currentProfit;
+                minSoFar = Math.Min(minSoFar, prices[i]);
+            }
+
+            return profit;
+        }
+    }
+}
