d4d291c776ab7dc199a3d69b6175d4b2aff39840
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-12 08:28:28 -0700

    LongestPalindromes

--- Program.cs
+++ Program.cs
@@ -4,5 +4,5 @@ using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
-string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
-StringProblems.GroupAnagrams(strs);
+string s = "cbbd";
+StringProblems.LongestPalindromes(s);
--- StringProblems.cs
+++ StringProblems.cs
@@ -8,6 +8,47 @@ namespace ConsoleApp1
 {
     internal class StringProblems
     {
+        public static string LongestPalindromes(string s)
+        {
+            string result = "";
+            int resLen = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int left;
+                int right;
+
+                left = i;
+                right = i;
+                while (left >= 0 && right < s.Length && s[left] == s[right])
+                {
+                    if (right - left + 1 > resLen)
+                    {
+                        result = s.Substring(left, right - left + 1);
+                        resLen = right - left + 1;
+                    }
+
+                    left = left - 1;
+                    right = right + 1;
+                }
+
+                left = i;
+                right = i + 1;
+                while (left >= 0 && right < s.Length && s[left] == s[right])
+                {
+                    if (right - left + 1 > resLen)
+                    {
+                        result = s.Substring(left, right - left + 1);
+                        resLen = right - left + 1;
+                    }
+
+                    left = left - 1;
+                    right = right + 1;
+                }
+
+
+            }
+            return result;
+        }
 
         public static IList<IList<string>> GroupAnagrams(string[] strs)
         {
