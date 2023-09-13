f941fadf6115ce672daf5301a2517f245c6939bd
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-13 11:50:30 -0700

    GraphAdjacencyListBFS

---
+++ GraphProblems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class GraphProblems
+    {
+        public static void GraphAdjacencyListBFS(int[][] graph)
+        {
+            Queue<int[]> queue = new Queue<int[]>();
+            var seen = new bool[graph.Length];
+            queue.Enqueue(graph[0]);
+            seen[0] = true;
+            Console.WriteLine(0);
+            while (queue.Count > 0)
+            {
+                var edges = queue.Dequeue();
+                foreach (var edge in edges)
+                {
+                    if (seen[edge] != true)
+                    {
+                        Console.WriteLine(edge);
+                        seen[edge] = true;
+                        queue.Enqueue(graph[edge]);
+                    }
+
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,5 +4,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
-string s = "AABABBA";
-Console.WriteLine(StringProblems.CharacterReplacement(s,1));
+int[][] graph = {
+new int[] {1, 3 },
+new int[] {0 },
+new int[] {3, 8 },
+new int[] {0, 2, 4, 5 },
+new int[] { 3, 6 },
+new int[] { 3 },
+new int[] { 4,7 },
+new int[] { 6 },
+new int[] { 2 } };
+GraphProblems.GraphAdjacencyListBFS(graph);
