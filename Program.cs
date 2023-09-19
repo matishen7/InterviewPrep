278dc0fc939efe835ef391f11d301c794c321ade
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-19 12:06:46 -0700

    WordDictionary

--- Program.cs
+++ Program.cs
@@ -4,5 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
-int[] nums = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
-Console.WriteLine(Arrays1D.LongestConsecutive(nums));
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.AddWord("bad");
+wordDictionary.AddWord("dad");
+wordDictionary.AddWord("mad");
+wordDictionary.Search("pa"); // return False
---
+++ WordDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class WordDictionary
+    {
+        private List<string> list;
+        public WordDictionary()
+        {
+            list = new List<string>();
+        }
+
+        public void AddWord(string word)
+        {
+            list.Add(word);
+        }
+
+        public bool Search(string word)
+        {
+            var filteredWords = list.FindAll(x => x.Length == word.Length);
+            return (filteredWords != null) ? true : false;
+        }
+    }
+}
