8a0cfd06f9b4bc936ee06605fda4eac1bd0d07cb
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-09-19 12:21:02 -0700

    WordDictionary

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using System.Net.Http.Headers;
 using System.Text;
 using static ConsoleApp1.BST;
 WordDictionary wordDictionary = new WordDictionary();
-wordDictionary.AddWord("bad");
-wordDictionary.AddWord("dad");
-wordDictionary.AddWord("mad");
-wordDictionary.Search("pa"); // return False
+wordDictionary.AddWord("at");
+wordDictionary.AddWord("and");
+wordDictionary.AddWord("an");
+wordDictionary.AddWord("add"); // return False
+wordDictionary.Search("."); // return False
--- WordDictionary.cs
+++ WordDictionary.cs
@@ -22,7 +22,31 @@ namespace ConsoleApp1
         public bool Search(string word)
         {
             var filteredWords = list.FindAll(x => x.Length == word.Length);
-            return (filteredWords != null) ? true : false;
+            if (filteredWords.Count == 0) return false;
+            bool[] wordMatch  = new bool[word.Length];
+            for (int i = 0; i < word.Length;i++)
+            {
+                if (word[i] == '.')
+                {
+                    wordMatch[i] = true;
+                    continue;
+                }
+                else
+                {
+                    for (int j = 0; j < filteredWords.Count; j++)
+                    {
+                        if (filteredWords[j][i] == word[i])
+                        {
+                            wordMatch[i] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < wordMatch.Length; i++)
+                if (wordMatch[i] == false) return false;
+            return true;
         }
     }
 }
